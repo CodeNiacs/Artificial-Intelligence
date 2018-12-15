7c3cf761a7806383f405a44ca67d127fcf178702
bijbrengen <slagter@bijbrengen.nl>
2018-12-15 18:52:33 +0100

    Moved challenge to warming up and added new

--- challenge/javascript/codeniacs_ai_javascript_challenge.js
+++ challenge/javascript/codeniacs_ai_javascript_challenge.js
@@ -1,2 +1,7 @@
-while(true){
+while ('Codeniacs' != window.prompt('Hoe heet dit project?')) {
+  window.alert('Fout antwoord!');
 }
+while ('Artificial Intelligence' != window.prompt('Hoe heet deze cursus?')) {
+  window.alert('Fout antwoord!');
+}
+window.alert('Klasse gedaan!');
